6d74e7f86274a32245ced23f287c31d35c0f42df
sbencoding <mcbenighost@gmail.com>
2019-12-23 22:36:59 +0100

    Added docs and updated typedef
    Added docs for new method to .verb.md
    Updated typdef file with the new method's signature

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,6 @@
 declare function timestamp(pattern?: string | Date, date?: Date): string
 declare function timestampUTC(pattern?: string | Date, date?: Date): string
+declare function setTimeZone(timezoneHoursOffset: Number): void
 
 export default timestamp
 export { timestampUTC as utc };
